a395c97aad62af91867400ced7edf0c52e26cae8
username_anjastabuni <reblytabuni2004@gmail.com>
2023-02-09 19:12:31 +0700

    desain web profil

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 function redirInstagram() {
   window.location.href = "https://instagram.com/mekal.tabuni";
 }
-{
-  /* function redirWhatsapp() {
-            window.location.href = "https://wa.me/+6281343109866"
-        } */
+
+function redirWhatsapp() {
+  window.location.href = "https://wa.me/+6281248069721";
+}
+function redirfacebook() {
+  window.location.href = "https://facebook.com/asther anjas";
+}
+function redirlinkend() {
+  window.location.href = "https://linkedin.com/reblytabuni22";
 }
